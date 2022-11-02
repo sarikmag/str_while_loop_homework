756e077404d5f7b2d4af2a716e323cf9632c1cbd
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-11-02 11:09:19 +0500

    aaa

--- While06.py
+++ While06.py
@@ -10,8 +10,9 @@ def main(s):
     i=0
     k=0
     while i<len(s):
-        if s[i]!='a' and s[i]!='e' and s[i]!='i' and s[i]!='u' and s[i]!='o':
+        
+        if s[i]!='a' and s[i]!='e' and s[i]!='i' and s[i]!='u' and s[i]!='o' and s[i]!='A' and s[i]!='E' and s[i]!='I' and s[i]!='U' and s[i]!='O':
             k+=1
         i+=1
     return k
-print(main("CodeschaeioolUz"))
+print(main("CodeschoolUz"))
