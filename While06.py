db6949fafcf1dde73790bbd19228103aba6b542d
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-11-02 11:20:38 +0500

    sdw

--- While06.py
+++ While06.py
@@ -1,3 +1,4 @@
+from string import punctuation
 def main(s):
     """
     A variable of type str is given. Find and return how many consonant letters there are.
@@ -7,12 +8,13 @@ def main(s):
     Returns:
         int: return answer
     """
+    c=punctuation
     i=0
     k=0
     while i<len(s):
-        
-        if s[i]!='a' and s[i]!='e' and s[i]!='i' and s[i]!='u' and s[i]!='o' and s[i]!='A' and s[i]!='E' and s[i]!='I' and s[i]!='U' and s[i]!='O':
-            k+=1
+        if c.find(s[i])==-1:
+            if s[i]!='a' and s[i]!='e' and s[i]!='i' and s[i]!='u' and s[i]!='o' and s[i]!='A' and s[i]!='E' and s[i]!='I' and s[i]!='U' and s[i]!='O':
+                k+=1
         i+=1
     return k
-print(main("CodeschoolUz"))
+print(main("fghfgh#%@efs"))
